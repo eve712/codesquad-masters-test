cdc1153c7268d331353ab9bb53924f4ebffa9c00
eve712 <62237639+eve712@users.noreply.github.com>
2020-12-11 23:06:20 +0900

    기능추가: 입력값을 의미로 나눠 배열 만드는  tokenize함수 작성

--- rubiks_cube.js
+++ rubiks_cube.js
@@ -1,19 +1,31 @@
 // ---------------------● Rubiks Cube Data ●-----------------------
 class RubiksCubeData {
-    constructor() {
-        sideU = new Array(3).fill(new Array(3).fill('B'));
-        sideD = new Array(3).fill(new Array(3).fill('R'));
-        sideL = new Array(3).fill(new Array(3).fill('W'));
-        sideR = new Array(3).fill(new Array(3).fill('G'));
-        sideF = new Array(3).fill(new Array(3).fill('O'));
-        sideB = new Array(3).fill(new Array(3).fill('Y'));
+    constructor(planeCubeData) {
+        this.sideU = new Array(3).fill(new Array(3).fill('B'));
+        this.sideD = new Array(3).fill(new Array(3).fill('R'));
+        this.sideL = new Array(3).fill(new Array(3).fill('W'));
+        this.sideR = new Array(3).fill(new Array(3).fill('G'));
+        this.sideF = new Array(3).fill(new Array(3).fill('O'));
+        this.sideB = new Array(3).fill(new Array(3).fill('Y'));
+        this.planeCubeData = planeCubeData;
+        this.inputArr = [];
     }
-    main() {
-
+    main(value) {
+        this.tokenize(value);
     }
     // 2단계 tokenize + 숫자value 변경 함수
-    getInputArr() {
-
+    tokenize(value) {
+        this.planeCubeData.tokenize.call(this, value);
+        const idx = [];
+        this.inputArr.forEach((el, i) => {if(this.isNumber(el)) idx.push(i); });
+        if(idx.length > 0) {
+            idx.forEach(idx => this.inputArr[idx - 1] += this.inputArr[idx])
+            this.inputArr = this.inputArr.filter(el => !this.isNumber(el));
+        }
+    }
+    isNumber(el) {
+        const bool = typeof parseInt(el) === 'number' && isFinite(parseInt(el));
+        return bool;
     }
     pickSideArr() {
 
@@ -25,22 +37,24 @@ class RubiksCubeData {
 
     }
     fixSideArr() {
-        
+
     }
  } 
 // ---------------------● View Rubiks Cube ●-----------------------
 class ViewRubiksCube {
-    constructor(reference) {
+    constructor(reference, rubiksCubeData) {
         this.step3Text = reference.step3Text;
         this.step3Btn = reference.step3Btn;
         this.step3Result = reference.step3Result;
+        this.rubiksCubeData = rubiksCubeData;
     }
     setEvent() {
-        this.step2Btn.addEventListener("click", this.handleData.bind(this));
+        this.step3Btn.addEventListener("click", this.handleData.bind(this));
     }
-    handleData() {
+    handleData(e) {
         e.preventDefault();
-        const value = this.step2Text.value;
+        const value = this.step3Text.value;
+        this.rubiksCubeData.main(value);
         this.viewAll();
     }
     //
@@ -58,6 +72,6 @@ class ViewRubiksCube {
     }
 }
 // -------------● 실행 ●-------------
-const rubiksCubeData = new RubiksCubeData();
-const viewRubiksCube = new ViewRubiksCube();
+const rubiksCubeData = new RubiksCubeData(planeCubeData);
+const viewRubiksCube = new ViewRubiksCube(reference, rubiksCubeData);
 viewRubiksCube.setEvent();
